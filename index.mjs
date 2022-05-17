109f4d4a249b22637a08b7fe617d410a4ab485e1
Kota Ninomiya <uv.ninomiya@gmail.com>
2022-05-17 12:44:44 +0900

    fix: remove crediential

--- index.mjs
+++ index.mjs
@@ -7,13 +7,13 @@ import { getFirestore, collection, query, orderBy, getDocs } from "firebase/fire
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAP2jT3SYXl4np8qPgN7po3SF1bAdaPxxE",
-  authDomain: "mil22-team-e.firebaseapp.com",
-  projectId: "mil22-team-e",
-  storageBucket: "mil22-team-e.appspot.com",
-  messagingSenderId: "97574635209",
-  appId: "1:97574635209:web:39de445e476d3b8d636f18",
-  measurementId: "G-D7035J4N5Y"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
